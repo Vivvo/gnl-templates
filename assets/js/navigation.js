e634f8789015e161f554aaf437407989308e19ea
gcahill <gcahill709@gmail.com>
2021-03-11 11:51:11 -0330

    reverting header changes

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -55,6 +55,11 @@
         var navClassParent = $('.gnl-navigation__nav').parent();
         var navID = "#";
 
+        if (navClassParent.hasClass('gnl-navigation--header')) {
+
+            $('.gnl-navigation__nav .gnl-navigation__nav-item:last-child').hide();
+        }
+
         $('.gnl-header__container').css("flex-direction", "row");
         $('.gnl-navigation-drawer__scrim').attr('onclick', '');
         displayDrawerMenuButton(navClassParent);
